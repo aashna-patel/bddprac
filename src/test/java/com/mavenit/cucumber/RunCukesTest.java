ec5ef9d40691cb2fb1d1d8a4042a3a9f7e34e939
aashna_patel@hotmail.com <Vedpatel19>
2020-02-28 23:38:11 +0000

    prac

--- src/test/java/com/mavenit/cucumber/RunCukesTest.java
+++ src/test/java/com/mavenit/cucumber/RunCukesTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources", tags = "@BackPackCapacityTest")
+@CucumberOptions(features = "src/test/resources", tags = "@Test", dryRun = true, strict = true)
 
 public class RunCukesTest {
 }
